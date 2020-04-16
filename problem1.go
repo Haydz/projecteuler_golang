a8705b1a51be03965e0523d314aae6c34306c325
Haydn Johnson <haydnit@gmail.com>
2020-04-16 19:03:59 -0400

    finished level 1

--- problem1.go
+++ problem1.go
@@ -9,21 +9,58 @@ package main
 
 import "fmt"
 
+// Equal tells whether a and b contain the same elements.
+// A nil argument is equivalent to an empty slice.
+
+func Contains(a []int, x int) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 
 	var threeMultiples []int
 	var fiveMultiples []int
 
-	for i := 1; i < 1000; i++ {
-		if (i * 3) < 1000 {
-			threeMultiples = append(threeMultiples, i*3)
-		}
-		if (i * 5) < 1000 {
-			fiveMultiples = append(fiveMultiples, i*5)
-		}
-
+	for i := 1; (i * 3) < 1000; i++ {
+		//fmt.Println(i)
+		threeMultiples = append(threeMultiples, i*3)
 	}
-	fmt.Println(threeMultiples)
-	fmt.Println(fiveMultiples)
+
+	for i := 1; (i * 5) < 1000; i++ {
+		//fmt.Println(i)
+		fiveMultiples = append(fiveMultiples, i*5)
+	}
+
+	//initial way of solving
+	// for i := 1; i < 1000; i++ {
+	// 	// if (i * 3) < 1000 {
+	// 	// 	threeMultiples = append(threeMultiples, i*3)
+	// 	// }
+	// 	if (i * 5) < 1000 {
+	// 		fiveMultiples = append(fiveMultiples, i*5)
+	// 	}
+
+	// }
+
+	//compare sum the difference of integers within the 2 slices
+	for _, value := range threeMultiples {
+		if Contains(fiveMultiples, value) == false {
+			fiveMultiples = append(fiveMultiples, value)
+
+		}
+	}
+
+	totalSum := 0
+
+	for _, value := range fiveMultiples {
+		fmt.Println("value: ", value)
+		totalSum = totalSum + value
+	}
+	fmt.Println(totalSum)
 
 }
